0653d194e53117fd100a7a59bdc0debe8b23725e
Nishat Anjum <nishatnanjum@gmail.com>
2016-11-09 19:17:14 -0500

    Uploaded Login Folder w/ the camera functionality

---
+++ LoginReg/LoginReg/ImageAddition.swift
@@ -0,0 +1,72 @@
+//
+//  ImageAddition.swift
+//  LoginReg
+//
+//  Created by Nishat Anjum on 10/24/16.
+//  Copyright © 2016 Locus. All rights reserved.
+//
+
+import UIKit
+
+class ImageAddition: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var pickedImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+        @IBAction func camerabuttonaction(_ sender:
+            UIButton) {
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+                imagePicker.allowsEditing = false
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            
+        }
+        
+        @IBAction func photoLibraryaction(_ sender:
+            UIButton) {
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+                imagePicker.allowsEditing = true
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+        }
+        
+        @IBAction func saveaction(_ sender:
+            UIButton) {
+            let imageData = UIImageJPEGRepresentation(pickedImage.image!, 0.6)
+            let compressedJPEGImage = UIImage(data: imageData!)
+            UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!,nil, nil, nil)
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!){
+            pickedImage.image = image
+            self.dismiss(animated: true, completion: nil)
+        }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
