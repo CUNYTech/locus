a7720b51aa042f90e201f5d94ed78faf10afab73
nishata <nishatnanjum@gmail.com>
2016-12-04 22:07:35 -0500

    Added floating menu to add *working pages* :nail_care:

--- Locus/AccountViewController.swift
+++ Locus/AccountViewController.swift
@@ -1,63 +1,27 @@
-//  ImageAddition.swift
-//  LoginReg
 //
-//  Created by Nishat Anjum on 10/24/16.
-//  Copyright © 2016 Locus. All rights reserved.
-// An app to visually chronologize and organize travel anecdotes for the modern explorer.
+//  AccountViewController.swift
+//  Locus
+//
+//  Created by Nishat Anjum on 12/4/16.
+//  Copyright © 2016 Elizabeth Kelly. All rights reserved.
+//
 
 import UIKit
 import KCFloatingActionButton
 
-class ImageAddition: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, KCFloatingActionButtonDelegate {
-    
-    @IBOutlet weak var pickedImage: UIImageView!
+class AccountViewController: UIViewController, KCFloatingActionButtonDelegate {
     
     var fab = KCFloatingActionButton()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         layoutFAB()
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func camerabuttonaction(_ sender:
-        UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
-        }
-        
-    }
-    
-    @IBAction func photoLibraryaction(_ sender:
-        UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
-            imagePicker.allowsEditing = true
-            self.present(imagePicker, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func saveaction(_ sender:
-        UIButton) {
-        let imageData = UIImageJPEGRepresentation(pickedImage.image!, 0.6)
-        let compressedJPEGImage = UIImage(data: imageData!)
-        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!,nil, nil, nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!){
-        pickedImage.image = image
-        self.dismiss(animated: true, completion: nil)
     }
     
     func layoutFAB() {
@@ -118,15 +82,16 @@ class ImageAddition: UIViewController, UIImagePickerControllerDelegate, UINaviga
     func KCFABClosed(_ fab: KCFloatingActionButton) {
         print("FAB Closed")
     }
+    
 
     /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
